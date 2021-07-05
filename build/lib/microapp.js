949f9a871fcb66d7826cc9feb173e59e109fc104
theHinneh <thehinneh@theHinnehs-MacBook-Pro.local>
2021-07-05 20:21:39 +0000

    logs

--- build/lib/microapp.js
+++ build/lib/microapp.js
@@ -2,32 +2,21 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.AyobaAppApi = void 0;
 require("browser-env")(["window", "navigator", "Android"]);
-var agent = navigator.userAgent;
-var vendor = navigator.vendor;
 var opera = window.opera;
-// import browserEnv from "browser-env";
-// browserEnv(["navigator"]);
-var AyobaApi = /** @class */ (function () {
-    function AyobaApi() {
+var AyobaAppApi = function () {
+    var userAgent = navigator.userAgent || navigator.vendor || opera;
+    console.log("userAgent", userAgent);
+    // Windows Phone must come first because its UA also contains "Android"
+    if (/windows phone/i.test(userAgent)) {
+        return null;
     }
-    //   Android: any;
-    AyobaApi.prototype.getAyoba = function () {
-        var userAgent = navigator.userAgent || navigator.vendor || opera;
-        console.log("userAgent", userAgent);
-        // Windows Phone must come first because its UA also contains "Android"
-        if (/windows phone/i.test(userAgent)) {
-            return null;
-        }
-        if (/android/i.test(userAgent)) {
-            return Android;
-        }
-        // iOS detection from: http://stackoverflow.com/a/9039885/177710
-        if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-            return null; // todo
-        }
-        return "unknown";
-    };
-    return AyobaApi;
-}());
-var Ayoba = new AyobaApi();
-exports.AyobaAppApi = Ayoba.getAyoba();
+    if (/android/i.test(userAgent)) {
+        return "Android";
+    }
+    // iOS detection from: http://stackoverflow.com/a/9039885/177710
+    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+        return null; // todo
+    }
+    return "unknown";
+};
+exports.AyobaAppApi = AyobaAppApi;
