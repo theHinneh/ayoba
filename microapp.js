dac6bab6f04f8925394efde3ad489628d6dd85b3
theHinneh <thehinneh@theHinnehs-MacBook-Pro.local>
2021-07-06 23:09:06 +0000

    Ayoba

--- microapp.js
+++ microapp.js
@@ -1,21 +1,21 @@
-function getAyoba() {
-  var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+// function getAyoba() {
+//   var userAgent = navigator.userAgent || navigator.vendor || window.opera;
 
-  // Windows Phone must come first because its UA also contains "Android"
-  if (/windows phone/i.test(userAgent)) {
-    return null;
-  }
+//   // Windows Phone must come first because its UA also contains "Android"
+//   if (/windows phone/i.test(userAgent)) {
+//     return null;
+//   }
 
-  if (/android/i.test(userAgent)) {
-    return Android;
-  }
+//   if (/android/i.test(userAgent)) {
+//     return Android;
+//   }
 
-  // iOS detection from: http://stackoverflow.com/a/9039885/177710
-  if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-    return null; // todo
-  }
+//   // iOS detection from: http://stackoverflow.com/a/9039885/177710
+//   if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+//     return null; // todo
+//   }
 
-  return "unknown";
-}
-var Ayoba = getAyoba();
-export default Ayoba;
+//   return "unknown";
+// }
+// var Ayoba = getAyoba();
+// export default Ayoba;
