55c92d007e10aa617badc0a7e5d2b9bc2f8d97e3
theHinneh <thehinneh@theHinnehs-MacBook-Pro.local>
2021-07-06 17:12:20 +0000

    typings

--- src/app/main.service.ts
+++ src/app/main.service.ts
@@ -2,7 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
-declare var getAyoba: any;
+
+// import * as Ayoba from 'getAyoba'
 
 @Injectable({
   providedIn: 'root',
