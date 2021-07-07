18914ac3a6e448111f416b495aa257fab2b28337
theHinneh <thehinneh@theHinnehs-MacBook-Pro.local>
2021-07-07 05:40:09 +0000

    import microapp

--- src/app/main.service.ts
+++ src/app/main.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { Ayoba } from './microapp.js';
+import Ayoba from './microapp.js';
 // declare var Ayoba: any;
 
 @Injectable({
