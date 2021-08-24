cdf0542181698c18cf12291bc0b9ee9f84702ce2
theHinneh <hinneh04@gmail.com>
2021-08-24 09:59:23 +0000

    base url

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,17 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
+  var old = console.log;
+  var logger: any = document.getElementById('log');
+  console.log = function (message) {
+    if (typeof message == 'object') {
+      logger.innerHTML += (JSON && JSON.stringify ? JSON.stringify(message) : message) + '<br />';
+    } else {
+      logger.innerHTML += message + '<br />';
+    }
+  }
   enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+  .catch((err: any) => console.error(err));
